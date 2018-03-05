8fe1a8875db475d758c03d5512c12733990721a5
hiimmario <m.reichart@hotmail.com>
2018-03-05 12:28:43 +0100

    first commit

---
+++ contracts/bettingContractV1.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.4;
+
+
+contract bettingContractV1 {
+  function bettingContractV1() {
+    // constructor
+  }
+}
---
+++ test/betting_contract_v1.js
@@ -0,0 +1,7 @@
+contract('bettingContractV1', function(accounts) {
+  it("should assert true", function(done) {
+    var betting_contract_v1 = bettingContractV1.deployed();
+    assert.isTrue(true);
+    done();
+  });
+});
