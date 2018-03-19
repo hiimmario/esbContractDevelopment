755e79770732fcc1be50fc20ce30dd4459a66930
hiimmario <m.reichart@hotmail.com>
2018-03-19 11:15:40 +0100

    receiving winner through oraclize

---
+++ contracts/bettingContractV2.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.11;
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+contract bettingContractV2 {
+
+    string public winner;
+
+    event LogWinnerUpdated(string winner);
+    event LogNewOraclizeQuery(string description);
+
+    function bettingContractV2() payable {
+        updateWinner();
+    }
+
+    function __callback(bytes32 myid, string result) {
+        require(msg.sender == oraclize_cbAddress());
+
+        winner = result;
+        LogWinnerUpdated(winner);
+    }
+
+    function updateWinner() payable {
+        if (oraclize_getPrice("URL") > this.balance) {
+            LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+        } else {
+            LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+            oraclize_query(60, "URL", "json(https://api.pandascore.co/matches?filter[id]=21835&token=xFEMHt5iIXzTaDydesV2fk01-L-YKp7OFLcZcWZJ4tav2Og-7jA).0.winner.name");
+        }
+    }
+
+    function getWinner() {
+        return winner;
+    }
+}
+
